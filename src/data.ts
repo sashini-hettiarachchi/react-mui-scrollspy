95496af9df5146fb622d1837141fef6151cd0a67
Sashini Hettiarachchi <sashini.hettiarachchi1@gmail.com>
2022-08-01 19:01:52 +0530

    add smaple data

---
+++ src/data.ts
@@ -0,0 +1,229 @@
+export const list = [
+  {
+    categoryName: "Noodles",
+    categoryId: 1,
+    items: [
+      {
+        id: "noodle_1",
+        name: "Egg Noodle",
+        image:
+          "https://static.toiimg.com/thumb/52467119.cms?width=1200&height=900",
+      },
+      {
+        id: "noodle_2",
+        name: "korean-stir-fried",
+        image:
+          "https://www.thespruceeats.com/thmb/72FrHLxd-Q1RQXjtFrVwroNOHs0=/1800x1012/smart/filters:no_upscale()/korean-stir-fried-2118794-Hero-37b94c729b554b9e992fc7e481c7ec05.jpg",
+      },
+      {
+        id: "noodle_3",
+        name: "Vegetable Hakka Noodles",
+        image:
+          "https://www.kannammacooks.com/wp-content/uploads/Vegetable-Hakka-Noodles-Recipe-1-3.jpg",
+      },
+      {
+        id: "noodle_4",
+        name: "Indian style chili garlic noodles",
+        image:
+          "https://holycowvegan.net/wp-content/uploads/2021/04/indian-style-chili-garlic-noodles-featured-image.jpg",
+      },
+      {
+        id: "noodle_5",
+        name: "Spicy Chicken Hakka Noodles",
+        image:
+          "https://www.cubesnjuliennes.com/wp-content/uploads/2020/06/Spicy-Chicken-Hakka-Noodles-Recipe.jpg",
+      },
+      {
+        id: "noodle_6",
+        name: "Glass Noodles Stir Fry",
+        image:
+          "https://khinskitchen.com/wp-content/uploads/2022/02/glass-noodles-stir-fry-05-500x500.jpg",
+      },
+      {
+        id: "noodle_7",
+        name: "Garlic Noodles",
+        image:
+          "https://hot-thai-kitchen.com/wp-content/uploads/2020/07/garlic-noodles.jpg",
+      },
+      {
+        id: "noodle_8",
+        name: "Salmon Noodles",
+        image:
+          "https://img.taste.com.au/ESOTTEOs/taste/2018/08/sept18_speedy-salmon-noodle-stirfry_taste_1980x1320-140540-1.jpg",
+      },
+      {
+        id: "noodle_9",
+        name: "Dan Dan Ish Noodles",
+        image:
+          "https://images.immediate.co.uk/production/volatile/sites/2/2019/01/Dan-Dan-ish-noodles-2964652.jpg?quality=90&resize=700,466",
+      },
+    ],
+  },
+  {
+    categoryName: "Burgers",
+    categoryId: 2,
+    items: [
+      {
+        id: "burger_1",
+        name: "Veg Burger",
+        image:
+          "https://www.vegrecipesofindia.com/wp-content/uploads/2020/12/burger-recipe-1-500x375.jpg",
+      },
+      {
+        id: "burger_2",
+        name: "Chicken Burger",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYAouKF8xCCwwVw43RVyYYpM2itTQ3ca6tTdGstDY8rgm_K0s-A4CpjhXLkCYPaAt7uhE&usqp=CAU",
+      },
+      {
+        id: "burger_3",
+        name: "Chicken Burger",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYAouKF8xCCwwVw43RVyYYpM2itTQ3ca6tTdGstDY8rgm_K0s-A4CpjhXLkCYPaAt7uhE&usqp=CAU",
+      },
+      {
+        id: "burger_4",
+        name: "Chicken Burger",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYAouKF8xCCwwVw43RVyYYpM2itTQ3ca6tTdGstDY8rgm_K0s-A4CpjhXLkCYPaAt7uhE&usqp=CAU",
+      },
+      {
+        id: "burger_5",
+        name: "Chicken Burger",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYAouKF8xCCwwVw43RVyYYpM2itTQ3ca6tTdGstDY8rgm_K0s-A4CpjhXLkCYPaAt7uhE&usqp=CAU",
+      },
+      {
+        id: "burger_6",
+        name: "Chicken Burger",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYAouKF8xCCwwVw43RVyYYpM2itTQ3ca6tTdGstDY8rgm_K0s-A4CpjhXLkCYPaAt7uhE&usqp=CAU",
+      },
+    ],
+  },
+  {
+    categoryName: "Rice",
+    categoryId: 3,
+    items: [
+      {
+        id: "rice_1",
+        name: "Chicken Biriyani",
+        image:
+          "https://i.pinimg.com/736x/24/ac/bb/24acbb7530fb22151d3f3f1581fb947d.jpg",
+      },
+      {
+        id: "rice_2",
+        name: "Nasi Goreng",
+        image:
+          "https://www.kitchensanctuary.com/wp-content/uploads/2020/07/Nasi-Goreng-square-FS-57.jpg",
+      },
+      {
+        id: "rice_3",
+        name: "Tomato Rice",
+        image:
+          "https://www.yumcurry.com/wp-content/uploads/2018/03/tomato-rice-recipe.jpg",
+      },
+      {
+        id: "burger_4",
+        name: "Lemon Rice",
+        image:
+          "https://www.indianhealthyrecipes.com/wp-content/uploads/2022/02/lemon-rice-recipe.jpg",
+      },
+      {
+        id: "burger_5",
+        name: "Garlic Rice",
+        image:
+          "https://www.lifesambrosia.com/wp-content/uploads/xGarlic-Rice-Recipe-Photo-2.jpg.pagespeed.ic.iXOgnZaALY.jpg",
+      },
+    ],
+  },
+  {
+    categoryName: "Dessert",
+    categoryId: 4,
+    items: [
+      {
+        id: "des_1",
+        name: "Chocolate Cake ",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTMXVLgZmRtqYhCuv8dUfOQZlJEyqxy35VS-TbCL2Dhk7G2-OH78jsWABgFwe1JiS9vDY&usqp=CAU",
+      },
+      {
+        id: "des_2",
+        name: "Apple Pie",
+        image:
+          "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F39%2F2020%2F10%2F12%2Fpecant-pie-RU337843.jpg&w=400&h=532&c=sc&poi=face&q=60",
+      },
+      {
+        id: "des_3",
+        name: "Donuts",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQouE4BbfY9DH0JSIux4ijyzDcRwz1gWAQmkw&usqp=CAU",
+      },
+      {
+        id: "des_4",
+        name: "Australian Pavlova",
+        image:
+          "https://sharepostt.com/wp-content/uploads/2022/06/Dessert-names-australian-pavlova.jpg",
+      },
+      {
+        id: "des_5",
+        name: "Dessert",
+        image:
+          "https://i.pinimg.com/originals/19/dd/2f/19dd2f4ca69fff80fb13c02d23fc7ddb.jpg",
+      },
+      {
+        id: "des_6",
+        name: "Waffle",
+        image:
+          "https://hips.hearstapps.com/del.h-cdn.co/assets/16/41/1476386922-belgian-waffle.jpg",
+      },
+      {
+        id: "des_7",
+        name: "Liege Waffles with Berries and Chocolate Syrup.",
+        image:
+          "https://insanelygoodrecipes.com/wp-content/uploads/2021/04/Liege-Waffles-with-Berries-and-Chocolate-Syrup.png",
+      },
+      {
+        id: "des_8",
+        name: "Waffle Chocolate Syrup White Chocolate",
+        image:
+          "https://i.pinimg.com/736x/0f/2f/fd/0f2ffdeb55af6a955364b39c8b4d7dbe--chocolate-syrup-white-chocolate.jpg",
+      },
+      {
+        id: "des_9",
+        name: "Dessert",
+        image:
+          "https://qph.cf2.quoracdn.net/main-qimg-b245abf38fb3971c1b9e2be713df3aca-lq",
+      },
+      {
+        id: "des_10",
+        name: "Chocolate Strawberry Jelly Roll",
+        image:
+          "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/10/28/0/FNK_Chocolate-Strawberry-Jelly-Roll_H1_s4x3.jpg.rend.hgtvcom.406.305.suffix/1572274398797.jpeg",
+      },
+      {
+        id: "des_11",
+        name: "Cake",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThpNwKmtaiU68MRQ_SjvDb38fhT2Fk9qyoHPiF2CdYL76ZN4LRI-Qf1aO5brKlGoIakF8&usqp=CAU",
+      },
+      {
+        id: "des_12",
+        name: "Simple Dessert",
+        image:
+          "https://cdn1.matadornetwork.com/blogs/1/2018/05/shutterstock_758819113-560x420.jpg",
+      },
+      {
+        id: "des_13",
+        name: "Simple Dessert Cake",
+        image: "https://static.dw.com/image/36105565_403.jpg",
+      },
+      {
+        id: "des_14",
+        name: "Simple Dessert",
+        image:
+          "https://i.pinimg.com/236x/53/a6/74/53a6744d1267a4dcd43cfd08ae7f1066.jpg",
+      },
+    ],
+  },
+];
