fe6f933812fef19a0811e8d169bf996ad785d443
Sashini Hettiarachchi <sashini.hettiarachchi1@gmail.com>
2022-08-02 21:25:07 +0530

    Add inital styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import './App.css';
+import "./App.css";
+import { list } from "./data";
 
 function App() {
-  
   return (
-    <div className="App">
-      <div>
-        <div>
-
+    <div className="app">
+      <div className="menuWrapper">
+        <div className="menuCategoryWrapper">
+          {list.map((category) => (
+            <div key={category.categoryId} className="menuCategories">{category.categoryName}</div>
+          ))}
+        </div>
+        <div className="menuBody">
+          {list.map((category) => (
+            <div key={category.categoryId} className="menuItemContainer">
+              <div className="menuCategoryName">{category.categoryName}</div>
+              <div className="menuItemWrapper">
+                {category.items.map((item) => (
+                  <div key={item.id} className="menuItem">
+                    <img src={item.image} alt={item.name} className="menuItemImage"/>
+                    <p>{item.name}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
