420bb2eb793251793c9b872ca1235d3e1b2cf1d0
Sashini Hettiarachchi <sashini.hettiarachchi1@gmail.com>
2022-08-19 23:37:57 +0530

    Add scrolling feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Tab, Tabs } from "@mui/material";
 import { useState } from "react";
-import { scrollIntoView } from "seamless-scroll-polyfill/lib/scrollIntoView";
+import { scrollIntoView } from "seamless-scroll-polyfill";
 import "./App.css";
 import { list } from "./data";
 
@@ -8,47 +8,50 @@ function App() {
 
   const [value, setValue] = useState(0);
 
-  const handleChange = (e, newValue) => {
+  const handleChange = (e : React.SyntheticEvent<Element, Event>, newValue : number) => {
     setValue(newValue);
-    scrollIntoView(document.getElementById(`menu_${newValue}`), {
+    if(document.getElementById(`menu_${newValue}`)){
+      scrollIntoView(document.getElementById(`menu_${newValue}`)!, {
       behavior: "smooth",
       block: "start",
       inline: "center",
     });
+    }
+    
   };
 
-  // const handleScroll = () => {
-  //     list.forEach((element, index) => {
-  //     const observer = new IntersectionObserver(
-  //       (entries) => {
-  //         const intersectedDiv = entries.find((entry) => entry.isIntersecting === true);
-  //         if (intersectedDiv) {
-  //           const target = intersectedDiv.target.id.split("_");
-  //           const intersectedDivIndex = target[1];
-  //           setValue(parseInt(intersectedDivIndex, 10));
-  //         }
-  //       },
-  //       { root: document.querySelector("#menuContainer"), threshold: 0.51 },
-  //     );
-  //     document.getElementById(`menu_${index}`) && observer.observe(document.getElementById(`menu_${index}`));
-  //   });
+  const handleScroll = () => {
+      list.forEach((element, index) => {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          const intersectedDiv = entries.find((entry) => entry.isIntersecting === true);
+          if (intersectedDiv) {
+            const target = intersectedDiv.target.id.split("_");
+            const intersectedDivIndex = target[1];
+            setValue(parseInt(intersectedDivIndex, 10));
+          }
+        },
+        { root: document.querySelector("#menuContainer"), threshold: 0.51 },
+      );
+      document.getElementById(`menu_${index}`) && observer.observe(document.getElementById(`menu_${index}`)!);
+    });
     
-  // };
+  };
 
 
   return (
     <div className="app">
       <div className="menuWrapper">
         <div className="menuCategoryWrapper">
-          {list.map((category) => (
-              <Tabs value={value}  aria-label="basic tabs example" onChange={handleChange}>
-              <Tab label={category.categoryName} className="menuCategories" ></Tab>
+          {list.map((category, index) => (
+            <Tabs value={value}  aria-label="basic tabs example" onChange={handleChange}>
+              <Tab label={category.categoryName} className="menuCategories" value={index}></Tab>
             </Tabs>
           ))}
         </div>
-        <div className="menuBody">
-          {list.map((category) => (
-            <div key={category.categoryId} className="menuItemContainer" id={`menu_${category.categoryId}`}>
+        <div className="menuBody" onScroll={handleScroll} id={"#menuContainer"}>
+          {list.map((category, index) => (
+            <div key={category.categoryId} className="menuItemContainer" id={`menu_${index}`}>
               <div className="menuCategoryName">{category.categoryName}</div>
               <div className="menuItemWrapper">
                 {category.items.map((item) => (
